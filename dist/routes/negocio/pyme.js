0f876d2b9fa8f2d13f2a3a3f433f3dc069a81707
imsamv <ismael.midence@unah.hn>
2023-10-29 21:48:50 -0600

    Creacion API Pyme Exitosamente

--- dist/routes/negocio/pyme.js
+++ dist/routes/negocio/pyme.js
@@ -7,6 +7,9 @@ const express_1 = require("express");
 const pyme_controller_1 = require("../../controllers/negocio/pyme.controller");
 const validarToken_1 = __importDefault(require("../validarToken"));
 const routerPyme = (0, express_1.Router)();
-routerPyme.get('/getAllPymes', validarToken_1.default, pyme_controller_1.getAllPymes); // obtiene todos los usuarios
-routerPyme.post('/getPyme', pyme_controller_1.getPyme); // obtiene el usuario especificado
+routerPyme.get('/getAllPymes', validarToken_1.default, pyme_controller_1.getAllPymes); // obtiene todas las Pymes
+routerPyme.post('/getPyme', pyme_controller_1.getPyme); // obtiene la Pyme especificada
+routerPyme.delete('/deletePyme', validarToken_1.default, pyme_controller_1.deletePyme); //Elimina la Pyme de la Base de Datos
+routerPyme.post('/updatePyme', validarToken_1.default, pyme_controller_1.updatePyme); //Actualiza la Pyme en la Base de Datos
+routerPyme.post('/postPyme', validarToken_1.default, pyme_controller_1.postPyme); //Inserta un nuevo Pyme en la Base de Datos
 exports.default = routerPyme;
