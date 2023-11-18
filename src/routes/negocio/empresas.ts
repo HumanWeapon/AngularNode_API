e71067d91b9d3c86e45d6084980857afaa2cbaa6
HumanWeapon <odair.umanzor@unah.hn>
2023-11-18 16:36:37 -0600

    cambios

--- src/routes/negocio/empresas.ts
+++ src/routes/negocio/empresas.ts
@@ -7,7 +7,7 @@ const routerEmpresa = Router()
 
 routerEmpresa.get('/getAllEmpresas',validarToken , getAllEmpresas); // Obtiene todas las Empresas
 routerEmpresa.post('/getEmpresa', getEmpresa); // Obtiene la Empresa especificada
-routerEmpresa.post('/postEmpresa', validarToken,postEmpresa);//Inserta una nueva Empresa en la Base de Datos
+routerEmpresa.post('/postEmpresa', validarToken, postEmpresa);//Inserta una nueva Empresa en la Base de Datos
 routerEmpresa.delete('/deleteEmpresa',validarToken,deleteEmpresa);//Elimina la Empresa de la Base de Datos
 routerEmpresa.post('/updateEmpresa', validarToken,updateEmpresa);//Actualiza la Pyme en la Base de Datos
 routerEmpresa.post('/inactivateEmpresa',validarToken, inactivateEmpresa);//Inactiva una Pyme en la DB
