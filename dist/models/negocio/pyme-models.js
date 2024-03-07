6d7c2dbae37fc521068943efba424d72add11dc8
HumanWeapon <odair.umanzor@unah.hn>
2024-03-06 22:24:44 -0600

    si falla es culpa de frankling

--- dist/models/negocio/pyme-models.js
+++ dist/models/negocio/pyme-models.js
@@ -6,25 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Pyme = void 0;
 const sequelize_1 = require("sequelize");
 const connection_1 = __importDefault(require("../../db/connection"));
-const tipoEmpresa_models_1 = require("./tipoEmpresa-models");
+const roles_models_1 = require("../roles-models");
 exports.Pyme = connection_1.default.define('pyme', {
     id_pyme: {
         type: sequelize_1.DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
-    id_tipo_empresa: {
-        type: sequelize_1.DataTypes.INTEGER,
-    },
     nombre_pyme: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false
     },
     rtn: {
-        type: sequelize_1.DataTypes.STRING,
-        allowNull: false
-    },
-    categoria: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false
     },
@@ -51,13 +44,17 @@ exports.Pyme = connection_1.default.define('pyme', {
     estado: {
         type: sequelize_1.DataTypes.NUMBER,
         allowNull: false
+    },
+    id_rol: {
+        type: sequelize_1.DataTypes.INTEGER,
+        allowNull: false
     }
 }, {
     tableName: 'tbl_me_pyme',
     schema: 'mipyme',
     timestamps: false
 });
-exports.Pyme.belongsTo(tipoEmpresa_models_1.tipoEmpresa, {
-    foreignKey: 'id_tipo_empresa',
-    as: 'tipoEmpresa' // Alias para la relación
+exports.Pyme.belongsTo(roles_models_1.Roles, {
+    foreignKey: 'id_rol',
+    as: 'rol', // Nombre del alias para la relación
 });
