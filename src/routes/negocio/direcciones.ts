d40c6f66272af492043a0c3a2f5ab544ffa439c6
HumanWeapon <odair.umanzor@unah.hn>
2024-03-07 22:35:09 -0600

    cambios en direcciones model

--- src/routes/negocio/direcciones.ts
+++ src/routes/negocio/direcciones.ts
@@ -1,16 +1,10 @@
 import {Router} from 'express';
 import validarToken from '../validarToken';
-import { activateDirecContactos, deleteDirecContactos, getAllDirecContactos, getDirecContactos, inactivateDirecContactos, postDirecContactos, updateDirecContactos } from '../../controllers/negocio/direcciones-controllers';
+import { getdirecciones } from '../../controllers/negocio/direcciones-controllers';
 
 
 const routerDireccionContacto = Router()
 
-routerDireccionContacto.get('/getAllDirecContactos',validarToken , getAllDirecContactos); // Obtiene todas las Empresas
-routerDireccionContacto.post('/getDirecContactos', getDirecContactos); // Obtiene la Empresa especificada
-routerDireccionContacto.post('/postDirecContactos', validarToken,postDirecContactos);//Inserta una nueva Empresa en la Base de Datos
-routerDireccionContacto.delete('/deleteDirecContactos',validarToken,deleteDirecContactos);//Elimina la Empresa de la Base de Datos
-routerDireccionContacto.post('/updateDirecContactos', validarToken,updateDirecContactos);//Actualiza la Pyme en la Base de Datos
-routerDireccionContacto.post('/inactivateDirecContactos',validarToken, inactivateDirecContactos);//Inactiva una Pyme en la DB
-routerDireccionContacto.post('/activateDirecContactos',validarToken, activateDirecContactos);//Activa una Pyme en la DB
+routerDireccionContacto.get('/getdirecciones',validarToken , getdirecciones); // Obtiene todas las Empresas
 
 export default routerDireccionContacto;
