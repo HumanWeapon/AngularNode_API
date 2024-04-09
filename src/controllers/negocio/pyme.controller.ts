1c1e3b282bbcba3e1cd93fb363153bc30ec8212b
HumanWeapon <odair.umanzor@unah.hn>
2024-04-09 01:40:54 -0600

    protección de rutas

--- src/controllers/negocio/pyme.controller.ts
+++ src/controllers/negocio/pyme.controller.ts
@@ -39,7 +39,7 @@ export const loginPyme = async (req: Request, res: Response) => {
         // Genera el token de autenticación
         const token = jwt.sign({
             pyme: pyme
-        }, process.env.SECERT_KEY_PYME || 'Lamers006*');
+        }, process.env.SECERT_KEY || 'Lamers005*');
             
         res.json(token);
     } catch (error) {
