1f3a9072834830dce403af1a83d99b241b171abc
HumanWeapon <odair.umanzor@unah.hn>
2024-04-09 00:40:18 -0600

    protección de rutas

--- src/routes/validarToken.ts
+++ src/routes/validarToken.ts
@@ -1,49 +1,48 @@
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const validarToken = ( req: Request, res: Response, next: NextFunction) => {
-    console.log('Validar Token')
+const validarToken = (req: Request, res: Response, next: NextFunction) => {
+    console.log('Validar Token');
     const headerToken = req.headers['authorization'];
 
-    if(headerToken != undefined && headerToken.startsWith('Bearer ')){
-        //Tiene Token
+    if (headerToken !== undefined && headerToken.startsWith('Bearer ')) {
+        // Tiene Token
         try {
             const bearerToken = headerToken.slice(7);
-            jwt.verify(bearerToken, process.env.SECRET_KEY|| 'Lamers005*')
+            jwt.verify(bearerToken, process.env.SECRET_KEY || 'Lamers005*');
+            next();
+        } catch (error) {
+            // Si el token no es válido, ejecuta validarTokenpyme
+            validarTokenpyme(req, res, next);
+            return;
+        }
+    } else {
+        res.status(401).json({
+            msg: 'Acceso denegado'
+        });
+    }
+};
+
+const validarTokenpyme = (req: Request, res: Response, next: NextFunction) => {
+    console.log('Validar Token PYME');
+    const headerToken = req.headers['authorization'];
+
+    if (headerToken !== undefined && headerToken.startsWith('Bearer ')) {
+        // Tiene Token
+        try {
+            const bearerToken = headerToken.slice(7);
+            jwt.verify(bearerToken, process.env.SECERT_KEY_PYME || 'Lamers006*');
             next();
         } catch (error) {
             res.status(401).json({
                 msg: 'Token no valido'
-            })
+            });
         }
-    }else{
+    } else {
         res.status(401).json({
             msg: 'Acceso denegado'
-        })
+        });
     }
-    
-}
-const validarTokenpyme = ( req: Request, res: Response, next: NextFunction) => {
-    console.log('Validar Token')
-    const headerToken = req.headers['authorization'];
-
-    if(headerToken != undefined && headerToken.startsWith('Bearer ')){
-        //Tiene Token
-        try {
-            const bearerToken = headerToken.slice(7);
-            jwt.verify(bearerToken, process.env.SECERT_KEY_PYME || 'Lamers006*')
-            next();
-        } catch (error) {
-            res.status(401).json({
-                msg: 'Token no valido'
-            })
-        }
-    }else{
-        res.status(401).json({
-            msg: 'Acceso denegado'
-        })
-    }
-    
-}
+};
 
 export { validarToken, validarTokenpyme };
