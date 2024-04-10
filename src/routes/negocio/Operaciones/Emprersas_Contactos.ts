ae8a2a8627aef52dff02deb800f05976a221c0fe
HumanWeapon <odair.umanzor@unah.hn>
2024-04-09 22:53:17 -0600

    cambios en obtener empresas activas

--- src/routes/negocio/Operaciones/Emprersas_Contactos.ts
+++ src/routes/negocio/Operaciones/Emprersas_Contactos.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import validarToken from '../../validarToken';
 import {  } from '../../../controllers/negocio/Operaciones/Empresas_Productos-controller';
-import { ReporteContactos, agregarOperacionEmpresaContacto, consultarContactosActivosporId, consultarContactosNoRegistradosPorId, eliminarOperacionEmpresaContacto } from '../../../controllers/negocio/Operaciones/Empresas_Contactos-controller';
+import { ReporteContactos, agregarOperacionEmpresaContacto, consultarContactosActivosporId, consultarContactosNoRegistradosPorId, consultarContactosporId, eliminarOperacionEmpresaContacto } from '../../../controllers/negocio/Operaciones/Empresas_Contactos-controller';
 
 const empresasContatos = Router();
 
 empresasContatos.get('/consultarContactosNoRegistradosPorId/:id', validarToken, consultarContactosNoRegistradosPorId);
-empresasContatos.get('/consultarContactosporId/:id', validarToken, consultarContactosActivosporId);
+empresasContatos.get('/consultarContactosporId/:id', validarToken, consultarContactosporId);
 empresasContatos.get('/consultarContactosActivosporId/:id', validarToken, consultarContactosActivosporId);
 empresasContatos.get('/ReporteContactos', validarToken, ReporteContactos);
 empresasContatos.post('/agregarOperacionEmpresaContacto', validarToken, agregarOperacionEmpresaContacto);
